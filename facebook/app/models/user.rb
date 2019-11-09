8310ef04183316b2c2d0a467f8e20342911b991a
Rodolfo Llinás <rllinasc@unal.edu.co>
2019-11-08 23:13:25 -0500

    users

--- facebook/app/models/user.rb
+++ facebook/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
+  validates :first_name, presence:true
+  validates :last_name, presence:true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- facebook/db/schema.rb
+++ facebook/db/schema.rb
@@ -10,11 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_202730) do
+ActiveRecord::Schema.define(version: 2019_10_28_203925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.bigint "requester_id"
+    t.bigint "requestee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["requestee_id", "requester_id"], name: "index_friend_requests_on_requestee_id_and_requester_id", unique: true
+    t.index ["requestee_id"], name: "index_friend_requests_on_requestee_id"
+    t.index ["requester_id"], name: "index_friend_requests_on_requester_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "requester_id"
+    t.integer "requestee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["requestee_id", "requester_id"], name: "index_friendships_on_requestee_id_and_requester_id", unique: true
+    t.index ["requester_id"], name: "index_friendships_on_requester_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,6 +47,8 @@ ActiveRecord::Schema.define(version: 2019_10_19_202730) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
